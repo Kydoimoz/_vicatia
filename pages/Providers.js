c33bf4c7f7aca9e0f7bb8ac4ea58da9414f0155a
Kydoimoz- <kydoimozv@gmail.com>
2024-01-17 15:14:24 +0100

    changed_v04

--- pages/Providers.js
+++ pages/Providers.js
@@ -1,5 +1,6 @@
 "use client";
+import React from "react";
 import { SessionProvider } from "next-auth/react";
-export const AuthProvider = ({ children }) => {
+export default AuthProvider = ({ children }) => {
     return <SessionProvider>{children}</SessionProvider>
 }
